30f418be6938e4f0de7374e59976cc3151106fb5
somebudyelse-ka <somebudyelse@proton.me>
2025-12-01 16:09:10 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get install qemu-system uml-utilities virt-manager git \
     wget libguestfs-tools p7zip-full make dmg2img tesseract-ocr \
-    tesseract-ocr-eng genisoimage vim net-tools screen -y
+    tesseract-ocr-eng genisoimage vim net-tools screen novnc python3-websockify python3-numpy -y
+openssl req -x509 -nodes -newkey rsa:3072 -keyout novnc.pem -out novnc.pem -days 3650
 ./fetch-macOS-v2.py
 dmg2img -i BaseSystem.dmg BaseSystem.img
 read -p "Hard Drive Size in GB (run df -h to see how much space is free): " s
