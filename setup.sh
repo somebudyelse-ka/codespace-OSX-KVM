2f31a972580aaf3a830d8ef41992fa5106306565
somebudyelse-ka <somebudyelse@proton.me>
2025-12-01 16:27:40 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,4 @@ sudo apt update; sudo apt install qemu-system uml-utilities virt-manager git \
 openssl req -x509 -nodes -newkey rsa:3072 -keyout novnc.pem -out novnc.pem -days 3650
 ./fetch-macOS-v2.py -s high-sierra
 dmg2img -i BaseSystem.dmg BaseSystem.img
-read -p "Hard Drive Size in GB (run df -h to see how much space is free): " s
-qemu-img create -f qcow2 mac_hdd_ng.img ${s}G
+qemu-img create -f qcow2 mac_hdd_ng.img $(( $(df -BG / | awk 'NR==2') - 1 ))G
